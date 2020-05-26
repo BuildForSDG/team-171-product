ee9b42c1004a4d426926c1cd84f5278d34d15cb9
antony_kariu <kariuantony@gmail.com>
2020-05-26 12:08:50 +0300

    refactor(Footer): Removes footer as application to use SPA architecture.
    Deletes all footer styles and components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Navbar from "../Navbar/Navbar";
-import Listing from "../Listing/Listing";
-import CreateListing from "../Listing/CreateListing";
+import "../../styles/home.scss";
 
-import Project from "../Project/Project";
-import CreateProject from "../Project/CreateProject";
-import Footer from "../Footer/Footer";
+const Listing = lazy(() => import("../Listing/Listing"));
+const CreateListing = lazy(() => import("../Listing/CreateListing"));
+const Project = lazy(() => import("../Project/Project"));
+const CreateProject = lazy(() => import("../Project/CreateProject"));
 
 const Home = () => {
   return(
     <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/listing" component={Listing} />
-        <Route path="/create-list" component={CreateListing} />
-        <Route path="/project" component={Project} />
-        <Route path="/create-project" component={CreateProject} />
-      </Switch>
-      <Footer />
+      <div className="container">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="dashboard">
+          <div className="sticky-nav">
+            <div className="search">
+              search
+            </div>          
+            <div className="toggle">toggle</div>
+          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/listing" component={Listing} />
+              <Route path="/create-list" component={CreateListing} />
+              <Route path="/project" component={Project} />
+              <Route path="/create-project" component={CreateProject} />
+            </Switch>
+          </Suspense>
+        </div>
+      </div>
     </Router>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { logoutUser } from "../../actions";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.scss";
 
 const Navbar = (props) => {
   const { isLoggingOut, logoutError, error, dispatch } = props; 
@@ -19,13 +20,11 @@ const Navbar = (props) => {
         <div className="job-listing">
           <Link to="/listing" style={{ textDecoration: "none" }} className="job-link"> 
             <p>Job Listing</p>
-            <p>Find your dream job on skillmatcher's job listing</p>
           </Link>          
         </div>
         <div className="projects">
           <Link to="/project" style={{ textDecoration: "none" }} className="job-link"> 
             <p>Freelance Projects</p>
-            <p>Search for projects and showcase your talent</p>
           </Link>
         </div>
       </div>
@@ -33,18 +32,15 @@ const Navbar = (props) => {
         <div className="create-job">
           <Link to="create-list" style={{ textDecoration: "none" }} className="job-link"> 
             <p>List job opening</p>
-            <p>Post a job and get Quality applications from our talent pool</p>
           </Link>
         </div>
         <div className="create-project">
           <Link to="/create-project" style={{ textDecoration: "none" }} className="job-link"> 
             <p>Post a Project</p>
-            <p>Post your project on skillmatcher and let us help you build your brand</p>
           </Link>
         </div>
         <div className="search-talent">
           <p>Search Talent</p>
-          <p>Search our databse for the specific role you need</p>
         </div>
       </div>
       <div className="profile">
